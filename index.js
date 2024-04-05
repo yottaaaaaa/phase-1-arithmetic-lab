43dbd1dbeb85e8c292c2449ce829d019b7c95897
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-04-05 19:50:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+let num1 =31
+let num2=2
+let multiply=(num1*num2)
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+let num3 = 40; 
+let num4 = 6;
+
+let mod = num3 % num4;
+
+console.log(mod);
+
+let max =20
+
+
